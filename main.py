59f92bc68d932fc1554f78e596fe0822b56be643
Dipankar Biswas <dbiswas@msn.com>
2025-10-06 16:19:06 -0700

    Minor changes

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from msft-agent-framework!")
+import asyncio
+from agent_framework import ChatAgent
+from agent_framework.azure import AzureAIAgentClient
+from azure.identity.aio import AzureCliCredential
 
+async def main():
+    async with (
+        AzureCliCredential() as credential,
+        ChatAgent(
+            chat_client=AzureAIAgentClient(async_credential=credential),
+            instructions="You are good at telling jokes."
+        ) as agent,
+    ):
+        result = await agent.run("Tell me a joke about a pirate.")
+        print(result.text)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
